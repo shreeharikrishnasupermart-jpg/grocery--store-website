c23fa751c4af0d05f616678de4ec6dadbb8680ac
shreeharikrishnasupermart-jpg <shreeharikrishnasupermart@gmail.com>
2026-09-07 14:40:16 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,27 @@
+const products = [
+  {
+    id: 1,
+    name: "Aashirvaad Atta",
+    brand: "Aashirvaad",
+    category: "grocery",
+    unit: "5 kg",
+    mrp: 320,
+    price: 285,
+    discount: 11,
+    stock: 20,
+    image: ""
+  },
+
+  {
+    id: 2,
+    name: "Amul Butter",
+    brand: "Amul",
+    category: "dairy",
+    unit: "500 g",
+    mrp: 290,
+    price: 275,
+    discount: 5,
+    stock: 15,
+    image: ""
+  }
+];
