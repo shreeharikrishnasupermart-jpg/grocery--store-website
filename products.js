21e65a2080acffb57ba4f387f8b78184dcb3fc3a
shreeharikrishnasupermart-jpg <shreeharikrishnasupermart@gmail.com>
2026-09-16 17:02:08 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,4 +1,12 @@
+/* =====================================================
+   SHREE HARIKRISHNA SUPERMART
+   PRODUCT DATABASE
+===================================================== */
+
 const products = [
+
+  /* ================= GROCERY ================= */
+
   {
     id: 1,
     name: "Aashirvaad Atta",
@@ -14,6 +22,48 @@ const products = [
 
   {
     id: 2,
+    name: "Tata Salt",
+    brand: "Tata",
+    category: "grocery",
+    unit: "1 kg",
+    mrp: 30,
+    price: 28,
+    discount: 7,
+    stock: 30,
+    image: ""
+  },
+
+  {
+    id: 3,
+    name: "Fortune Sunflower Oil",
+    brand: "Fortune",
+    category: "grocery",
+    unit: "1 L",
+    mrp: 155,
+    price: 145,
+    discount: 6,
+    stock: 20,
+    image: ""
+  },
+
+  {
+    id: 4,
+    name: "India Gate Basmati Rice",
+    brand: "India Gate",
+    category: "grocery",
+    unit: "5 kg",
+    mrp: 650,
+    price: 599,
+    discount: 8,
+    stock: 12,
+    image: ""
+  },
+
+
+  /* ================= DAIRY ================= */
+
+  {
+    id: 5,
     name: "Amul Butter",
     brand: "Amul",
     category: "dairy",
@@ -23,5 +73,213 @@ const products = [
     discount: 5,
     stock: 15,
     image: ""
+  },
+
+  {
+    id: 6,
+    name: "Amul Cheese Slices",
+    brand: "Amul",
+    category: "dairy",
+    unit: "200 g",
+    mrp: 145,
+    price: 135,
+    discount: 7,
+    stock: 12,
+    image: ""
+  },
+
+  {
+    id: 7,
+    name: "Amul Paneer",
+    brand: "Amul",
+    category: "dairy",
+    unit: "200 g",
+    mrp: 95,
+    price: 90,
+    discount: 5,
+    stock: 10,
+    image: ""
+  },
+
+
+  /* ================= BAKERY ================= */
+
+  {
+    id: 8,
+    name: "Harvest White Bread",
+    brand: "Harvest",
+    category: "bakery",
+    unit: "400 g",
+    mrp: 45,
+    price: 40,
+    discount: 11,
+    stock: 15,
+    image: ""
+  },
+
+  {
+    id: 9,
+    name: "Britannia Fruit Cake",
+    brand: "Britannia",
+    category: "bakery",
+    unit: "250 g",
+    mrp: 80,
+    price: 72,
+    discount: 10,
+    stock: 10,
+    image: ""
+  },
+
+  {
+    id: 10,
+    name: "Milk Rusk",
+    brand: "Local",
+    category: "bakery",
+    unit: "200 g",
+    mrp: 60,
+    price: 55,
+    discount: 8,
+    stock: 10,
+    image: ""
+  },
+
+
+  /* ================= SNACKS ================= */
+
+  {
+    id: 11,
+    name: "Balaji Wafers Masala",
+    brand: "Balaji",
+    category: "snacks",
+    unit: "50 g",
+    mrp: 10,
+    price: 10,
+    discount: 0,
+    stock: 50,
+    image: ""
+  },
+
+  {
+    id: 12,
+    name: "Balaji Simply Salted",
+    brand: "Balaji",
+    category: "snacks",
+    unit: "50 g",
+    mrp: 10,
+    price: 10,
+    discount: 0,
+    stock: 50,
+    image: ""
+  },
+
+  {
+    id: 13,
+    name: "Kurkure Masala Munch",
+    brand: "Kurkure",
+    category: "snacks",
+    unit: "90 g",
+    mrp: 20,
+    price: 20,
+    discount: 0,
+    stock: 40,
+    image: ""
+  },
+
+  {
+    id: 14,
+    name: "Lays Magic Masala",
+    brand: "Lays",
+    category: "snacks",
+    unit: "50 g",
+    mrp: 20,
+    price: 20,
+    discount: 0,
+    stock: 40,
+    image: ""
+  },
+
+
+  /* ================= BEVERAGES ================= */
+
+  {
+    id: 15,
+    name: "Tata Tea Gold",
+    brand: "Tata Tea",
+    category: "beverages",
+    unit: "250 g",
+    mrp: 145,
+    price: 135,
+    discount: 7,
+    stock: 15,
+    image: ""
+  },
+
+  {
+    id: 16,
+    name: "Bournvita",
+    brand: "Cadbury",
+    category: "beverages",
+    unit: "500 g",
+    mrp: 270,
+    price: 249,
+    discount: 8,
+    stock: 10,
+    image: ""
+  },
+
+  {
+    id: 17,
+    name: "Real Fruit Juice",
+    brand: "Real",
+    category: "beverages",
+    unit: "1 L",
+    mrp: 120,
+    price: 110,
+    discount: 8,
+    stock: 15,
+    image: ""
+  },
+
+
+  /* ================= HOUSEHOLD ================= */
+
+  {
+    id: 18,
+    name: "Vim Dishwash Bar",
+    brand: "Vim",
+    category: "household",
+    unit: "200 g",
+    mrp: 25,
+    price: 23,
+    discount: 8,
+    stock: 20,
+    image: ""
+  },
+
+  {
+    id: 19,
+    name: "Surf Excel Matic",
+    brand: "Surf Excel",
+    category: "household",
+    unit: "2 kg",
+    mrp: 390,
+    price: 355,
+    discount: 9,
+    stock: 10,
+    image: ""
+  },
+
+  {
+    id: 20,
+    name: "Harpic Toilet Cleaner",
+    brand: "Harpic",
+    category: "household",
+    unit: "500 ml",
+    mrp: 110,
+    price: 99,
+    discount: 10,
+    stock: 15,
+    image: ""
   }
+
 ];
