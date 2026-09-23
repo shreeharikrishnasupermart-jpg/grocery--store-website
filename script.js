87082619537861dd256023c29b2b62c0ab13dcec
shreeharikrishnasupermart-jpg <shreeharikrishnasupermart@gmail.com>
2026-09-24 01:39:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const GOOGLE_SHEET_CSV_URL =
 async function loadProductsFromGoogleSheet() {
   try {
     const response = await fetch(GOOGLE_SHEET_CSV_URL);
-
+    console.log("Google Sheet fetch started");
     if (!response.ok) {
       throw new Error("Google Sheet could not be loaded");
     }
